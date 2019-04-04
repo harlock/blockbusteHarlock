bc1be1fa6b4fd18653727e21ad4b7ef7aeb2515c
CPrimero <iscprimerocesar@gmail.com>
2019-04-04 14:37:59 -0600

    inicio proyecto

---
+++ resources/views/prueba/index.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/app.css" type="text/css">
+    <title>Document</title>
+</head>
+<body>
+
+    <main class="container">
+        <div class="row">
+            <h1 class="col-8 offset-2 text-primary text-center">
+                {{$msg}}
+            </h1>
+        </div>
+    </main>
+</body>
+</html>
